b8c74908cb0398a6abf4024bd8c9421d6a626ccf
Aisha Najeeb <90481018+ayshanjb25@users.noreply.github.com>
2024-06-05 05:52:44 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: true, 
+  output: 'export', // Add this line to enable static export
+  distDir: 'build', // Specify the output directory for the export
 }
 
 module.exports = nextConfig
