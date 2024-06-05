b9a313e0806db8ca18893db7b69cccf08ad81f0f
Aisha Najeeb <90481018+ayshanjb25@users.noreply.github.com>
2024-06-05 05:42:02 +0530

    layout and navbar

--- pages/about/index.js
+++ pages/about/index.js
@@ -17,99 +17,79 @@ import {
 
 
 //  data
-const aboutData = [
-  {
-    title: 'skills',
-    info: [
-      {
-        title: 'Web Development',
-        icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
-        ],
-      },
-      {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
-    ],
-  },
-  {
-    title: 'awards',
-    info: [
-      {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
-      },
-      {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'experience',
-    info: [
-      {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
-      },
-      {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
-      },
-      {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'credentials',
-    info: [
-      {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
-      },
-      {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
-      },
-      {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
-      },
-    ],
-  },
-];
+// const aboutData = [
+//   {
+//     title: 'skills',
+//     info: [
+//       {
+//         title: 'Web Development',
+//         icons: [
+//           <FaHtml5 />,
+//           <FaCss3 />,
+//           <FaJs />,
+//           <FaReact />,
+//           <SiNextdotjs />,
+//           <SiFramer />,
+//           <FaWordpress />,
+//         ],
+//       },
+//       {
+//         title: 'UI/UX Design',
+//         icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+//       },
+//     ],
+//   },
+//   {
+//     title: 'awards',
+//     info: [
+//       {
+//         title: 'Webby Awards - Honoree',
+//         stage: '2011 - 2012',
+//       },
+//       {
+//         title: 'Adobe Design Achievement Awards - Finalist',
+//         stage: '2009 - 2010',
+//       },
+//     ],
+//   },
+//   {
+//     title: 'experience',
+//     info: [
+//       {
+//         title: 'UX/UI Designer - XYZ Company',
+//         stage: '2012 - 2023',
+//       },
+//       {
+//         title: 'Web Developer - ABC Agency',
+//         stage: '2010 - 2012',
+//       },
+//       {
+//         title: 'Intern - DEF Corporation',
+//         stage: '2008 - 2010',
+//       },
+//     ],
+//   },
+//   {
+//     title: 'credentials',
+//     info: [
+//       {
+//         title: 'Web Development - ABC University, LA, CA',
+//         stage: '2011',
+//       },
+//       {
+//         title: 'Computer Science Diploma - AV Technical Institute',
+//         stage: '2009',
+//       },
+//       {
+//         title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
+//         stage: '2006',
+//       },
+//     ],
+//   },
+// ];
 
 const About = () => {
-  return (<div>
-    {aboutData.map((section, sectionIndex) => (
-      <div key={sectionIndex}>
-        <h2>{section.title}</h2>
-        {section.info.map((infoItem, infoIndex) => (
-          <div key={infoIndex}>
-            <h3>{infoItem.title}</h3>
-            {infoItem.icons ? (
-              <div>
-                {infoItem.icons.map((icon, iconIndex) => (
-                  <span key={iconIndex}>{icon}</span>
-                ))}
-              </div>
-            ) : (
-              <p>{infoItem.stage}</p>
-            )}
-          </div>
-        ))}
-      </div>
-    ))}
-  </div>);
+  return <div>About</div>;
 };
 
 export default About;
